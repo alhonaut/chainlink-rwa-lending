075e336bc9f457ef8bb5a417fe8270657c4a93eb
Andriy Shvets <103654086+alhonaut@users.noreply.github.com>
2024-11-06 19:42:59 +0200

    Update RealEstatePriceDetails.sol

--- rwa-token/src/RealEstatePriceDetails.sol
+++ rwa-token/src/RealEstatePriceDetails.sol
@@ -7,9 +7,7 @@ import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/l
 import {FunctionsSource} from "./FunctionsSource.sol";
 
 /**
- * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
- * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
+ * Non-audited contract.
  */
 contract RealEstatePriceDetails is FunctionsClient, FunctionsSource, OwnerIsCreator {
     using FunctionsRequest for FunctionsRequest.Request;
